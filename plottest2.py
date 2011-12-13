cb25bc8d549616d395fae7741ed2db49db65256b
Bastian Venthur <venthur@debian.org>
2011-12-13 12:49:03 +0100

    Made all plots in one single plot (get 27FPS)

--- plottest2.py
+++ plottest2.py
@@ -7,35 +7,31 @@ import time
 import threading
 
 from matplotlib import pyplot as plt
+import numpy as np
 
+PAST_POINTS = 100
 
 def main(fig):
-    #ax = plt.subplot(111)
-    ax = []
+    ax = plt.subplot(111)
     for i in range(16):
-        ax.append(plt.subplot(16, 1, i))
-        ax[i].plot(0)
+        ax.plot(0)
     fig.canvas.draw()
-    x_values = []
-    y_values = []
+    x_values = np.array([])
+    y_values = np.array([])
     for i in itertools.count():
         t = time.time()
         # set x/y data for the plot
-        x_values.append(i)
-        x_values = x_values[-9:]
-        y_values.append(math.sin(i))
-        y_values = y_values[-9:]
+        x_values = np.append(x_values[-PAST_POINTS:], i)
+        y_values = np.append(y_values[-PAST_POINTS:], math.sin(i))
         # plot the data
-        t = time.time()
-        for a in ax:
-            a.lines[0].set_data(x_values, y_values)
-            a.draw_artist(a.lines[0])
-            a.relim()
-            a.autoscale()
-            #a.clear()
-            #a.plot(x_values, y_values)
+        j = 0
+        for line in ax.lines:
+            line.set_data(x_values, y_values + j*10)
+            ax.draw_artist(line)
+            j += 1
+        plt.ylim(-10, j*10+10)
+        plt.xlim(i-PAST_POINTS, i)
         fig.canvas.draw()
-#        time.sleep(1)
         print 1 / (time.time() - t)
 
 if __name__ == '__main__':
