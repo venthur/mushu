fe1104405b254236a057c2186010f2952a13d802
Bastian Venthur <venthur@debian.org>
2012-01-11 12:22:57 +0100

    meh

--- plottest.py
+++ plottest.py
@@ -17,7 +17,7 @@ import gtec
 
 PAST_POINTS = 256
 CHANNELS = 17
-SCALE = 30000 #10 
+SCALE = 3000 #10 
 
 fig = plt.figure()
 
@@ -31,33 +31,30 @@ def main():
         ax.plot(0)
     fig.canvas.draw()
     x_values = np.array([])
-    y_values = [np.array([]) for i in range(CHANNELS)]
-    buffer = []
+    data_buffer = []
     data = []
     for i in itertools.count():
         t = time.time()
 
-        #buffer.extend(amp.get_data())
-        #print buffer
-        #data = buffer[:CHANNELS]
-        #buffer = buffer[CHANNELS:]
-        data = amp.get_data()
+        #data_buffer = np.append(data_buffer, amp.get_data())
+        data_buffer = amp.get_data()
+
+        mask = (len(data_buffer) / CHANNELS) * CHANNELS
+        #print 'truncating data with holds more than 17 channesl'
+        data = np.append(data, data_buffer[:mask])
+        data = data[-PAST_POINTS*CHANNELS:]
+        data_buffer = data_buffer[mask:]
+        data2 = np.reshape(data, (-1, CHANNELS))
         #print data
-        for i in range(CHANNELS):
-            y_values[i] = np.append(y_values[i], data[i::CHANNELS])
-            #y_values[i] = np.append(y_values[i], data[i*32:(i+1)*32:])
-            y_values[i] = y_values[i][-PAST_POINTS:]
-        data = []
-        # set x/y data for the plot
-        x_values = np.append(x_values[-PAST_POINTS:], i)
+        data2 = data2[-PAST_POINTS:]
         # plot the data
-        j = 0
+        j = CHANNELS - 1
         for line in ax.lines:
             #line.set_data(x_values, y_values[j] + j*210)
-            line.set_xdata([i for i in range(len(y_values[j]))])
-            line.set_ydata(y_values[j]/8.15 + j*SCALE)
+            line.set_xdata([i for i in range(len(data2))])
+            line.set_ydata(data2[:,j]/8.15 + j*SCALE)
             ax.draw_artist(line)
-            j += 1
+            j -= 1
         #ax.relim()
         #ax.autoscale()
         plt.ylim(-SCALE, CHANNELS*SCALE)
