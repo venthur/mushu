adbea7bebadf042570444c809a7cc885cc42c3ed
Bastian Venthur <venthur@debian.org>
2012-10-17 12:05:55 +0200

    Added context manager to wrap start and stop

--- amps/amplifier.py
+++ amps/amplifier.py
@@ -1,5 +1,53 @@
 
 class Amplifier(object):
+    """Amplifier base class.
+
+    How an amplifier should be used:
+
+        amp = Amp()
+        # measure impedance
+        amp.configure(config)
+        amp.start()
+        while 1:
+            data = amp.get_data()
+            if enough:
+                break
+        amp.stop()
+        # measure data
+        amp.configure(config)
+        amp.start()
+        while 1:
+            data = amp.get_data()
+            if enough:
+                break
+        amp.stop()
+
+    or using the context manager:
+
+        amp = Amp()
+        # measure impedance
+        amp.configure(config)
+        with amp as a:
+            while 1:
+                data = a.get_data()
+                if enough:
+                    break
+        # measure impedance
+        amp.configure(config)
+        with amp as a:
+            while 1:
+                data = a.get_data()
+                if enough:
+                    break
+
+    """
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.stop()
 
     def configure(self, config):
         """Configure the amplifier.
