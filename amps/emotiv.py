0ea74b896891e912b3c0e467591c9d217fb793bf
Bastian Venthur <venthur@debian.org>
2012-10-23 16:07:39 +0200

    Some cosmetic changes to the code.

--- amps/emotiv.py
+++ amps/emotiv.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 
-
 from Crypto.Cipher import AES
 import numpy as np
 
@@ -17,31 +16,27 @@ PRODUCT_ID = 0xed02
 ENDPOINT_IN = usb.util.ENDPOINT_IN | 2  # second endpoint
 
 
-
 class Epoc(Amplifier):
 
     def __init__(self):
+        # find amplifier
         self.dev = usb.core.find(idVendor=VENDOR_ID, idProduct=PRODUCT_ID)
         if self.dev is None:
             raise RuntimeError('Emotiv device is not connected.')
-
         # pyusb docs say you *have* to call set_configuration, but it does not
         # work unless i *don't* call it.
         #dev.set_configuration()
-
+        # get the serial number
         serial = usb.util.get_string(self.dev, 17, self.dev.iSerialNumber)
-
+        # claim the device and it's two interfaces
         if self.dev.is_kernel_driver_active(0):
             self.dev.detach_kernel_driver(0)
         usb.util.claim_interface(self.dev, 0)
-
         if self.dev.is_kernel_driver_active(1):
             self.dev.detach_kernel_driver(1)
         usb.util.claim_interface(self.dev, 1)
-
         # prepare AES
         self.cipher = AES.new(self.generate_key(serial, True))
-
 
     def get_data(self):
         try:
@@ -57,7 +52,6 @@ class Epoc(Amplifier):
             data = np.array()
         return data.reshape(-1, 18)
 
-
     def generate_key(self, sn, research=True):
         """Generate the encryption key.
 
@@ -67,18 +61,18 @@ class Epoc(Amplifier):
         """
         if research:
             key = ''.join([sn[15], '\x00', sn[14], '\x54',
-                        sn[13], '\x10', sn[12], '\x42',
-                        sn[15], '\x00', sn[14], '\x48',
-                        sn[13], '\x00', sn[12], '\x50'])
+                           sn[13], '\x10', sn[12], '\x42',
+                           sn[15], '\x00', sn[14], '\x48',
+                           sn[13], '\x00', sn[12], '\x50'])
         else:
             key = ''.join([sn[15], '\x00', sn[14], '\x48',
-                        sn[13], '\x00', sn[12], '\x54',
-                        sn[15], '\x10', sn[14], '\x42',
-                        sn[13], '\x00', sn[12], '\x50'])
+                           sn[13], '\x00', sn[12], '\x54',
+                           sn[15], '\x10', sn[14], '\x42',
+                           sn[13], '\x00', sn[12], '\x50'])
         return key
 
-
     def decrypt(self, raw):
+        """Decrypt a raw package."""
         data = self.cipher.decrypt(raw[:16]) + self.cipher.decrypt(raw[16:])
         tmp = 0
         for i in range(32):
@@ -87,6 +81,8 @@ class Epoc(Amplifier):
         return tmp
 
     def parse_raw(self, raw):
+        """Parse raw data."""
+        # TODO: Handle battery and counter correctly
         data = []
         shift = 256
         # counter / battery
@@ -121,4 +117,3 @@ if __name__ == '__main__':
         except Exception as e:
             print e
         break
-
