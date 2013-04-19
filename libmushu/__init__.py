f68ffc1f1072f20720dbc4c86e2bd1dc31bd21af
Bastian Venthur <venthur@debian.org>
2013-04-19 16:40:04 +0200

    Replaced Thread with Process.
    This is necessary because the get_data from the underlying amp driver
    might use C calls which don't release the GIL. In this case the Thread
    which collects the data from the socket is paused and can therefore not
    correctly timestamp the incoming markers. This is not the case for a
    process which runs independently from the GIL.

--- libmushu/__init__.py
+++ libmushu/__init__.py
@@ -4,7 +4,7 @@ import select
 import socket
 import time
 from threading import Thread, Lock
-
+from multiprocessing import Process, Queue, Event
 
 END_MARKER = '\n'
 BUFSIZE = 2**16
@@ -33,26 +33,29 @@ class AmpDecorator():
         if filename is not None:
             self.fh = open(filename, 'bw')
         # start the marker server
-        self.shared = {'running' : True,
-                       'time': time.time(),
-                       'block' : 0,
-                       'lock' : Lock(),
-                       'markers' : []
-                       }
-        self.tcp_reader = Thread(target=tcp_reader, args=(self.shared, ))
+        self.marker_queue = Queue()
+        self.tcp_reader_running = Event()
+        self.tcp_reader_running.set()
+        tcp_reader_ready = Event()
+        self.tcp_reader = Process(target=tcp_reader, args=(self.marker_queue, self.tcp_reader_running, tcp_reader_ready ))
         self.tcp_reader.start()
-        time.sleep(1)
+        logger.debug('Waiting for TCP reader to become ready...')
+        tcp_reader_ready.wait()
+        logger.debug('TCP reader is ready...')
         # start the amp
+        self.time = time.time()
         self.amp.start()
 
     def stop(self):
         # stop the amp
         self.amp.stop()
         # stop the marker server
-        self.shared['running'] = False
+        self.tcp_reader_running.clear()
+        logger.debug('Waiting for TCP reader process to stop...')
         self.tcp_reader.join()
+        logger.debug('TCP reader process stopped.')
         # close the files
-        self.fh.close()
+        #self.fh.close()
 
     def configure(self, config):
         self.amp.configure(config)
@@ -64,22 +67,28 @@ class AmpDecorator():
         # get data and marker from underlying amp
         data, marker = self.amp.get_data()
         # get tcp marker and merge them with markers from the amp
-        with self.shared['lock']:
-            time_old = self.shared['time']
-            self.shared['time'] = time.time()
-            self.shared['block'] = self.shared['block'] + 1
+        self.time_old = self.time
+        self.time = time.time()
+        # / lock
         # merge markers
         # duration of the block / #samples gives the length of a sample
         # independently from the current sampling frequency.
-        delta_t = self.shared['time'] - time_old
+        delta_t = self.time - self.time_old
         samples = len(data)
         t_sample = delta_t / samples
-        tcp_marker = map(lambda x: [int(x[1] // t_sample), x[2]], self.shared['markers'])
-        self.shared['markers'] = []
+        tcp_marker = []
+        while not self.marker_queue.empty():
+            m = self.marker_queue.get()
+            tcp_marker.append(m)
+        for m in tcp_marker:
+            delay = (m[0] - float(m[1])) * 1000
+            if delay > .5:
+                logger.warning("Marker delay: %.4fms" % delay)
+            #logger.debug("Marker delay: %.4fms" % delay)
         marker = sorted(marker + tcp_marker)
         # save data to files
-        if marker:
-            logger.debug(marker)
+        #if marker:
+        #    logger.debug(marker)
         return data, marker
 
     def get_channels(self):
@@ -87,45 +96,48 @@ class AmpDecorator():
 
 
 
-def tcp_reader(shared):
+def tcp_reader(queue, running, ready):
     # setup the server socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.setblocking(0)
     server_socket.bind(('localhost', PORT))
     server_socket.listen(5)
+    ready.set()
     # read the socket until 'running' is set to false
     rlist, wlist, elist = [server_socket], [], []
-    while shared['running']:
+    while running.is_set():
         readables, _, _ = select.select(rlist, wlist, elist, 1)
-        delta_t  = time.time() - shared['time']
-        with shared['lock']:
-            for sock in readables:
-                if sock == server_socket:
-                    # a new connection is opened
-                    connection, address = sock.accept()
-                    logger.debug('Incoming connection from %s', address)
-                    rlist.append(connection)
-                    data_buffer = ''
+        t = time.time()
+        for sock in readables:
+            if sock == server_socket:
+                # a new connection is opened
+                connection, address = sock.accept()
+                logger.debug('Incoming connection from %s', address)
+                rlist.append(connection)
+                data_buffer = ''
+            else:
+                data = sock.recv(BUFSIZE)
+                if not data:
+                    # an open connection was closed
+                    logger.debug('Connection closed')
+                    sock.close()
+                    rlist.remove(sock)
                 else:
-                    data = sock.recv(BUFSIZE)
-                    if not data:
-                        # an open connection was closed
-                        logger.debug('Connection closed')
-                        sock.close()
-                        rlist.remove(sock)
-                    else:
-                        # received maybe incomplete data from a
-                        # connection
-                        data_buffer = ''.join([data_buffer, data])
-                        split = data_buffer.rsplit(END_MARKER, 1)
-                        messages = []
-                        if len(split) > 1:
-                            # data_buffer contains at least one complete
-                            # message
-                            data_buffer = split[1]
-                            messages = split[0].split(END_MARKER)
-                            shared['markers'].extend([[shared['block'], delta_t, m] for m in messages])
+                    # received maybe incomplete data from a
+                    # connection
+                    data_buffer = ''.join([data_buffer, data])
+                    split = data_buffer.rsplit(END_MARKER, 1)
+                    messages = []
+                    if len(split) > 1:
+                        # data_buffer contains at least one complete
+                        # message
+                        data_buffer = split[1]
+                        messages = split[0].split(END_MARKER)
+                        for m in messages:
+                            queue.put([t, m])
+    logger.debug('Terminated select-loop')
+    sock.close()
 
 def get_available_amps():
     available_amps = []
