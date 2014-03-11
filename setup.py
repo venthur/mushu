48d0da5cfc43c89d4e3fae4add970bf9d6f8b6c5
Bastian Venthur <venthur@debian.org>
2014-03-11 14:03:00 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,37 @@
 
 from distutils.core import setup
 
+import libmushu
 
 setup(
     name = 'Mushu',
-    version = '0.0',
+    version = libmushu.__version__,
     description = 'Brain Computer Interfacing signal acquisition software.',
+    long_description = 'A Python library for BCI signal acquisition. It can be used as a stand-alone application or as a library.',
     author = 'Bastian Venthur',
     author_email = 'bastian.venthur@tu-berlin.de',
-    url = 'http://github.com/venthur/mushu/',
-    packages = ['libmushu', 'libmushu.amps'],
+    url = 'http://venthur.github.io/mushu/',
+    download_url = 'http://github.com/venthur/mushu/',
+    license = 'GPL2',
+    platforms = 'any',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Healthcare Industry',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: GPL2 License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Education',
+        'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.',
+        'Topic :: Software Development :: Libraries',
+        ],
+    packages = ['libmushu', 'libmushu.driver'],
     scripts = ['mushu.py'],
 )
 
