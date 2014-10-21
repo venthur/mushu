185a038fb5abc2f5bbfb628fb1de8b217e08f090
Bastian Venthur <venthur@debian.org>
2014-10-21 09:21:57 +0200

    Moved pull the samples after the markers.

--- libmushu/driver/labstreaminglayer.py
+++ libmushu/driver/labstreaminglayer.py
@@ -92,8 +92,8 @@ class LSLAmp(Amplifier):
         first sample of that block.
 
         """
-        samples, timestamps = self.lsl_inlet.pull_chunk(timeout=0.0, max_samples=self.max_samples)
         markers, m_timestamps = self.lsl_marker_inlet.pull_chunk(timeout=0.0, max_samples=self.max_samples)
+        samples, timestamps = self.lsl_inlet.pull_chunk(timeout=0.0, max_samples=self.max_samples)
         # flatten the output of the lsl markers, which has the form
         # [[m1], [m2]]
         markers = [i for sublist in markers for i in sublist]
